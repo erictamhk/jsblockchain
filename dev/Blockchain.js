11a9c4bb43193cccc701c225110ecca0b8227410
eric tam <eric.m2hk@gmail.com>
2019-01-20 19:31:41 +0800

    add hashBlock

--- dev/Blockchain.js
+++ dev/Blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require("sha256");
+
 class Blockchain {
   constructor() {
     this.chain = [];
@@ -34,6 +36,13 @@ class Blockchain {
     this.pendingTransactions.push(newTransaction);
 
     return this.getLastBlock()["index"] + 1;
+  }
+
+  hashBlock(previousBlockHash, currentBlockData, nonce) {
+    const dataAsString =
+      previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
   }
 }
 
