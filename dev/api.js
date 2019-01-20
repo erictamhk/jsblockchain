71b2520649144fa5fb63778a5cdc84e0b7c16aa6
eric tam <eric.m2hk@gmail.com>
2019-01-20 22:00:42 +0800

    test the api

--- dev/api.js
+++ dev/api.js
@@ -2,12 +2,16 @@ const express = require("express");
 const app = express();
 
 app.get("/blockchain", function(req, res) {
-  res.send("Hello World!!");
+  res.send("get blockchain!!");
 });
 
-app.post("/transaction", function(req, res) {});
+app.post("/transaction", function(req, res) {
+  res.send("post transaction!!");
+});
 
-app.get("/mine", function(req, res) {});
+app.get("/mine", function(req, res) {
+  res.send("get mine!!");
+});
 
 app.listen(3001, function() {
   console.log("Listening on port 3000...");
