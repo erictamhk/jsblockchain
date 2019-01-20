93a17b76938b8e39996fd25eaa7fdbd3dfa0f13f
eric tam <eric.m2hk@gmail.com>
2019-01-20 19:22:27 +0800

    testing the createNewTransaction function

--- dev/test.js
+++ dev/test.js
@@ -2,6 +2,16 @@ const Blockchain = require("./Blockchain");
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389, "iheiodeo", "djoij");
+bitcoin.createNewBlock(1, "A", "B");
+
+bitcoin.createNewTransaction(100, "userA", "userB");
+
+bitcoin.createNewBlock(2, "B", "C");
+
+bitcoin.createNewTransaction(101, "userA", "userB");
+bitcoin.createNewTransaction(102, "userB", "userC");
+bitcoin.createNewTransaction(103, "userC", "userD");
 
 console.log(bitcoin);
+
+console.log(bitcoin.chain[1]);
